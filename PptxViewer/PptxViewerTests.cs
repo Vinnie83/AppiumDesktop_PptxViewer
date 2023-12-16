5c0f6685ce2ceb8853c8829cde33f0dfc0e056bf
Vinnie83 <122921599+Vinnie83@users.noreply.github.com>
2023-12-16 11:38:22 +0200

    Added test

--- PptxViewer/PptxViewerTests.cs
+++ PptxViewer/PptxViewerTests.cs
@@ -1,7 +1,9 @@
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Android;
 using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Support.UI;
 
 namespace PptxViewer
 {
@@ -12,12 +14,14 @@ namespace PptxViewer
         private WindowsDriver<WindowsElement> driver;
         private AppiumOptions options;
         
+        
         [SetUp]
         public void Setup()
         {
             this.options = new AppiumOptions();
             options.AddAdditionalCapability("app", appIdentifier);
             this.driver = new WindowsDriver<WindowsElement>(new Uri(appiumServer),options);
+            
 
         }
 
@@ -31,7 +35,34 @@ namespace PptxViewer
         [Test]
         public void Test_OpenPptxFile()
         {
-            Assert.Pass();
+            var openFile = driver.FindElementByName("Open File..");
+            openFile.Click();
+
+            Thread.Sleep(3000); 
+
+            var inputFileName = driver.FindElementByClassName("Edit");
+            inputFileName.SendKeys("08.Security-Testing");
+
+            Thread.Sleep(1000);
+
+            var openButton = driver.FindElementByClassName("Button");
+            openButton.Click();
+
+            Thread.Sleep(10000);
+
+            var allSlides = driver.FindElementsByAccessibilityId("ImagesGridView");
+
+            foreach (var slide in allSlides)
+            {
+                string slideText = slide.Text;
+                if (slideText.Contains("Security Testing", StringComparison.OrdinalIgnoreCase))
+                {
+                    Assert.That(slideText.Contains("Security Testing", StringComparison.OrdinalIgnoreCase));
+                    break;
+                }
+            }   
+
+
         }
     }
 }
